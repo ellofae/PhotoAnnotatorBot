86b2e6f8f3d810df290088a28a16270e931c400f
ellofae <sergei.bykovskiy2003@gmail.com>
2023-08-16 01:33:57 +0300

    update

--- main.py
+++ main.py
@@ -36,12 +36,27 @@ def add_message(image, text, font, text_color, text_start_height, fontsize, spac
     #     y_text += fontsize*spacing
         
     #return y_text
-    print(fontsize*spacing*math.ceil((len(text) / max_char_count)))
+    print(y_text + fontsize*spacing*math.ceil((len(text) / max_char_count)))
     return y_text + fontsize*spacing * math.ceil((len(text) / max_char_count))
     
+def get_marking_data(message_sender):
+    current_time = datetime.now()
+    
+    date = current_time.strftime('%Y-%m-%d')
+    timer = current_time.strftime('%H:%M')
+    
+    text = f'@{message_sender}, {date}, {timer}'
+    return text
 
-def make_wrapper(font, fontsize, text, image, spacing, padding_bottom):
-    sender_mark_space = 45 + padding_bottom
+def get_extra_space_size(message_sender, font, image, fontsize, spacing):
+    text = get_marking_data(message_sender)
+    text, max_char_count = get_text_wrapper(font, image, text)
+    
+    return fontsize*spacing * math.ceil((len(text) / max_char_count)) * 3
+    
+
+def make_wrapper(font, fontsize, text, image, spacing, padding_bottom, padding_top, extra_space_size):
+    sender_mark_space = extra_space_size + padding_bottom + padding_top
     
     #lines = textwrap.wrap(text, width=image.width // 10)
     
@@ -56,24 +71,29 @@ def make_wrapper(font, fontsize, text, image, spacing, padding_bottom):
     
     return wrapper, extra_bound_size
 
-def sender_mark(image_wrapper, message_sender, text_start, font, text_color, padding_left):
-    current_time = datetime.now()
-    text_top_padding = 5
+
+
+def sender_mark(image_wrapper, message_sender, text_start, font, text_color, padding_left, fontsize,spacing):
+    #current_time = datetime.now()
+    #text_top_padding = 5
     
-    date = current_time.strftime('%Y-%m-%d')
-    timer = current_time.strftime('%H:%M')
+    # date = current_time.strftime('%Y-%m-%d')
+    # timer = current_time.strftime('%H:%M')
     
-    text = f'@{message_sender}, {date}, {timer}'
+    # text = f'@{message_sender}, {date}, {timer}'
+    text = get_marking_data(message_sender)
     #lines = textwrap.wrap(text, width=image_wrapper.width//10)
     
     draw_img = ImageDraw.Draw(image_wrapper)
     
-    y_text = text_start + text_top_padding
+    #y_text = text_start # + text_top_padding
     # for line in lines:
     #     draw_img.text((padding_left, y_text), 
     #               line, font=font, fill=text_color)
     #     y_text += fontsize*spacing
-    text, _ = get_text_wrapper(font, image_wrapper, text)
+    text, max_char_count = get_text_wrapper(font, image_wrapper, text)
+    y_text = text_start + fontsize*spacing * math.ceil((len(text) / max_char_count))
+    
     draw_img.text((padding_left, y_text), text, font=font, fill=text_color, align='left')
 
 # def get_dynamic_fontsize(image, text, font_name):
@@ -106,16 +126,19 @@ def main():
     padding_top = 10
     padding_bottom = 10
     padding_left = 15
+    text_top_padding = 10
     spacing = 1
     
     image = Image.open(f'./images/{image_name}')
     #font, fontsize = get_dynamic_fontsize(image, text, font_name)
     
-    image_wrapper, bound_size = make_wrapper(font, fontsize, text, image, spacing, padding_bottom)
+    extra_space_size = get_extra_space_size(message_sender, font, image, fontsize, spacing)
+    
+    image_wrapper, bound_size = make_wrapper(font, fontsize, text, image, spacing, padding_bottom, padding_top, extra_space_size)
     text_start_height = image_wrapper.height - int(bound_size) + padding_top
     
     text_end_line = add_message(image_wrapper, text, font, text_color, text_start_height, fontsize, spacing, padding_left)
-    sender_mark(image_wrapper, message_sender, text_end_line + padding_top, font, text_color, padding_left)
+    sender_mark(image_wrapper, message_sender, text_end_line + text_top_padding, font, text_color, padding_left, fontsize, spacing)
     
     image_wrapper.save(f'./results/{image_name}')
 
